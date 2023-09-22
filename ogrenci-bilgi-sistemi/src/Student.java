e990fb0da44f3df36d27d0e5ff4d0a7f97fed267
Gamze Sakarya <126356427+GamzeSakarya@users.noreply.github.com>
2023-09-22 12:43:21 +0300

    Add files via upload

---
+++ ogrenci-bilgi-sistemi/src/Student.java
@@ -0,0 +1,71 @@
+public class Student {
+    String name, stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+    Student(String name, int classes, String stuNo, Course mat, Course fizik, Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+    public void addBulkExamNote(int matNote, int fizikNote, int kimyaNote) {
+        if (matNote >= 0 && matNote <= 100) {
+            this.mat.note = matNote;
+        }
+
+        if (fizikNote >= 0 && fizikNote <= 100) {
+            this.fizik.note = fizikNote;
+        }
+
+        if (kimyaNote >= 0 && kimyaNote <= 100) {
+            this.kimya.note = kimyaNote;
+        }
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.avarage = (this.mat.note * (1 - this.mat.verbalWeight / 100) +
+                this.mat.verbalNote * (this.mat.verbalWeight / 100) +
+                this.fizik.note * (1 - this.fizik.verbalWeight / 100) +
+                this.fizik.verbalNote * (this.fizik.verbalWeight / 100) +
+                this.kimya.note * (1 - this.kimya.verbalWeight / 100) +
+                this.kimya.verbalNote * (this.kimya.verbalWeight / 100)) / 3;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote() {
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+}
